31b891dbb42bfd3c50bedd48b0aa584d90cc4e7d
francisco de asís domínguez iceta <ciscoadiz@gmail.com>
2026-05-08 13:25:13 +0100

    fix(either-mon): añadir @JsonProperty en ErrorResponse y FieldViolation para native image

--- either-mon/src/main/java/es/kitti/mon/error/FieldViolation.java
+++ either-mon/src/main/java/es/kitti/mon/error/FieldViolation.java
@@ -1,13 +1,14 @@
 package es.kitti.mon.error;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.Map;
 
 public record FieldViolation(
-        String field,
-        String code,
-        @JsonInclude(JsonInclude.Include.NON_EMPTY) Map<String, Object> params
+        @JsonProperty("field") String field,
+        @JsonProperty("code")  String code,
+        @JsonInclude(JsonInclude.Include.NON_EMPTY) @JsonProperty("params") Map<String, Object> params
 ) {
     public FieldViolation(String field, String code) {
         this(field, code, Map.of());
