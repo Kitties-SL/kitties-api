4943fe3b5bb3b6c64dcb7e4fdae4ced57d73ddb8
francisco de asís domínguez iceta <ciscoadiz@gmail.com>
2026-05-20 03:08:48 +0100

    feat(gateway-service): ruta pública POST /api/users/password/forgot

--- gateway-service/src/main/java/es/kitti/gateway/filter/JwtAuthFilter.java
+++ gateway-service/src/main/java/es/kitti/gateway/filter/JwtAuthFilter.java
@@ -19,6 +19,7 @@ public class JwtAuthFilter {
             "POST:/api/auth/logout",
             "POST:/api/users",
             "POST:/api/users/activate",
+            "POST:/api/users/password/forgot",
             "POST:/api/users/password/reset",
             "POST:/api/organizations/register",
             "GET:/doc",
