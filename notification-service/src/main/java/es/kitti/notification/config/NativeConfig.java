9d678a272780cf356302edd451e7487d4835b48b
francisco de asís domínguez iceta <ciscoadiz@gmail.com>
2026-05-20 03:08:52 +0100

    feat(notification-service): consumer y email de password-reset-requested

--- notification-service/src/main/java/es/kitti/notification/config/NativeConfig.java
+++ notification-service/src/main/java/es/kitti/notification/config/NativeConfig.java
@@ -2,12 +2,14 @@ package es.kitti.notification.config;
 
 import es.kitti.notification.event.AdoptionFormAnalysedEvent;
 import es.kitti.notification.event.PasswordChangedEvent;
+import es.kitti.notification.event.PasswordResetRequestedEvent;
 import es.kitti.notification.event.UserRegisteredEvent;
 import io.quarkus.runtime.annotations.RegisterForReflection;
 
 @RegisterForReflection(targets = {
         AdoptionFormAnalysedEvent.class,
         PasswordChangedEvent.class,
+        PasswordResetRequestedEvent.class,
         UserRegisteredEvent.class
 })
 public class NativeConfig {}
