12beb585660ad8dde4661643ac5ba67c45ecf4f7
francisco de asís domínguez iceta <ciscoadiz@gmail.com>
2026-05-27 17:11:14 +0100

    chore(notification): register new types in NativeConfig
    Add Notification, NotificationType, NotificationResponse,
    UnreadCountResponse and ErrorResponse to @RegisterForReflection.

--- notification-service/src/main/java/es/kitti/notification/config/NativeConfig.java
+++ notification-service/src/main/java/es/kitti/notification/config/NativeConfig.java
@@ -1,6 +1,11 @@
 package es.kitti.notification.config;
 
+import es.kitti.mon.error.ErrorResponse;
 import es.kitti.notification.client.IpGeolocationClient;
+import es.kitti.notification.dto.NotificationResponse;
+import es.kitti.notification.dto.UnreadCountResponse;
+import es.kitti.notification.entity.Notification;
+import es.kitti.notification.entity.NotificationType;
 import es.kitti.notification.event.AdoptionFormAnalysedEvent;
 import es.kitti.notification.event.PasswordChangedEvent;
 import es.kitti.notification.event.PasswordResetRequestedEvent;
@@ -12,6 +17,11 @@ import io.quarkus.runtime.annotations.RegisterForReflection;
         PasswordChangedEvent.class,
         PasswordResetRequestedEvent.class,
         UserRegisteredEvent.class,
-        IpGeolocationClient.GeoResponse.class
+        IpGeolocationClient.GeoResponse.class,
+        Notification.class,
+        NotificationType.class,
+        NotificationResponse.class,
+        UnreadCountResponse.class,
+        ErrorResponse.class
 })
 public class NativeConfig {}
